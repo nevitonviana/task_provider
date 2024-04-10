c26a5bc9fd5d6a66b8d8dcf2e5e51f96eb03566a
Neviton Viana <nevitonviana1@hotmail.com>
2024-04-10 19:54:25 -0300

    criação de um observador para fecha a conexão com o banco de dados

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
 import 'modules/splash/spash_page.dart';
+import 'sqlite_adm_connection.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(sqliteAdmConnection);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
